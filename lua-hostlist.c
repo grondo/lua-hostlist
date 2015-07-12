ba404e92901249dbbde33e4556b49732ea809fe8
Mark A. Grondona <mgrondona@llnl.gov>
2015-07-12 08:38:21 -0700

    add wrapper for luaL_setfuncs on Lua 5.1
    For Lua 5.2 support, we need to convert to luaL_setfuncs() for
    registration of hostlist functions and metatable methods.
    Use the method suggested on the Lua wiki and wrap luaL_setfuncs.

--- lua-hostlist.c
+++ lua-hostlist.c
@@ -824,18 +824,40 @@ static const struct luaL_Reg hostlist_iterator_methods [] = {
     { NULL,         NULL                          }
 };
 
+/*
+ *  Wrapper for luaL_setfuncs () on Lua 5.1
+ *  Taken from Lua 5.2
+ */
+#if NEED_LUAL_SETFUNS
+void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup)
+{
+    luaL_checkstack(L, nup+1, "too many upvalues");
+    for (; l->name != NULL; l++) {  /* fill the table with given functions */
+        int i;
+        lua_pushstring(L, l->name);
+        for (i = 0; i < nup; i++)  /* copy upvalues to the top */
+            lua_pushvalue(L, -(nup+1));
+        lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
+        lua_settable(L, -(nup + 3));
+    }
+    lua_pop(L, nup);  /* remove upvalues */
+}
+#endif /* NEED_LUAL_SETFUNCS */
+
+
 int luaopen_hostlist (lua_State *L)
 {
-	luaL_newmetatable (L, "Hostlist");
+    luaL_newmetatable (L, "Hostlist");
 
     /*  Register hostlist methods in metatable */
-    luaL_register (L, NULL, hostlist_methods);
+    luaL_setfuncs (L, hostlist_methods, 0);
 
     luaL_newmetatable (L, "HostlistIterator");
-    luaL_register (L, NULL, hostlist_iterator_methods);
+    luaL_setfuncs (L, hostlist_iterator_methods, 0);
 
     /*  Register hostlist public table functions: */
-	luaL_register (L, "hostlist", hostlist_functions);
+    lua_newtable (L);
+    luaL_setfuncs (L, hostlist_functions, 0);
 
     return 1;
 }
